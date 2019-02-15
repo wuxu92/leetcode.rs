8a92535a933dde4a1ca28c31ac4e35d8e5932f0b
wuxu92 <wuxu92@qq.com>
2019-02-15 16:42:53 +0800

    add one pass algorithm for sort_colors_75

--- src/sort_colors_75.rs
+++ src/sort_colors_75.rs
@@ -1,6 +1,7 @@
 // @url https://leetcode.com/problems/sort-colors/
 
-pub fn sort_colors(nums: &mut Vec<i32>) {
+// two-pass counting method
+pub fn sort_colors_1(nums: &mut Vec<i32>) {
     let mut count = [0,0,0];
     for i in nums.iter() { count[*i as usize] += 1; }
     let mut idx = 0;
@@ -8,6 +9,25 @@ pub fn sort_colors(nums: &mut Vec<i32>) {
         for _ in 0..count[i] { nums[idx] = i as i32; idx+=1;}
     }
 }
+
+// one-pass using swap and const space
+pub fn sort_colors(nums: &mut Vec<i32>) {
+    if nums.len() <= 1 { return }
+    let mut l = 0;
+    let mut h = nums.len()-1;
+    while l < h {
+        if nums[l] == 0 { l += 1; continue; }
+        if nums[h] == 2 { h -= 1; continue; }
+        let mut mid = l;
+        while mid <= h {
+            if nums[mid] == 0 { nums.swap(l, mid); l += 1; break;}
+            if nums[mid] == 2 { nums.swap(mid, h); h -= 1; break;}
+            mid += 1;
+        }
+        if mid > h { break; } // 没有需要交换的元素
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
