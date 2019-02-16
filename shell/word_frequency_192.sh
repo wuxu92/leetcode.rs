7bb6065e0fce984a128185778bfb8dddf09c63e9
wuxu92 <wuxu92@qq.com>
2019-02-16 15:55:28 +0800

    update word_frequency_192 beat 100%

--- shell/word_frequency_192.sh
+++ shell/word_frequency_192.sh
@@ -1,4 +1,6 @@
 # @url https://leetcode.com/problems/word-frequency/
 # Read from the file words.txt and output the word frequency list to stdout.
 
-xargs <<< "a a b" | tr ' ' '\n' | sort | uniq -c | sort -r | awk '{print $2" "$1}'
+# both pass
+# xargs < words.txt | tr ' ' '\n' | sort | uniq -c | sort -r | awk '{print $2" "$1}'
+tr ' ' '\n' < words.txt | sort | uniq -c | sort -nr | awk 'NF>1{print $2" "$1}'
