84a3d5aa035d6d24f92ddfc97b000b9a2110948d
wuxu92 <wuxu92@qq.com>
2019-02-16 16:37:38 +0800

    finish transpose_file_194.sh

---
+++ shell/transpose_file_194.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# @url https://leetcode.com/problems/transpose-file/
+
+# Read from the file file.txt and print its transposed content to stdout.
+
+# awk '{name=name FS $1; age=age FS $2}; END{print name"\n"age}' ./194_file.txt | sed 's/^ //'
+
+file="file.txt"
+file="194_file.txt2"
+read -r cols < <(head -n 1 "$file" | awk '{print NF}')
+
+for i in $(seq 1 $cols); do
+    awk -v i=$i '{print $i}' "$file" | xargs
+done
