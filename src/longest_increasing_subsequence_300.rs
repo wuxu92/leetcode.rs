d47b4d3efeb6047b30af0ea0dccfe730149eb891
wuxu92 <wuxu92@qq.com>
2019-02-26 20:41:40 +0800

    add soluton for longest_increasing_subsequence_300 with O(n*logn)

--- src/longest_increasing_subsequence_300.rs
+++ src/longest_increasing_subsequence_300.rs
@@ -1,7 +1,8 @@
 // @url https://leetcode.com/problems/longest-increasing-subsequence/
 
+// simple dp
 #[allow(dead_code)]
-pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+pub fn length_of_lis_01(nums: Vec<i32>) -> i32 {
     if nums.len() == 0 { return 0 }
     let mut dp : Vec<i32> = vec![1; nums.len()+1];
     let mut max;
@@ -18,6 +19,28 @@ pub fn length_of_lis(nums: Vec<i32>) -> i32 {
         if max < dp[i] { max = dp[i]; }
     }
     max
+}
+
+#[allow(dead_code)]
+pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+    let mut sv : Vec<i32> = vec![];
+    sv.reserve(nums.len());
+
+    for num in nums {
+        if sv.len() == 0 { sv.push(num); continue }
+        if num > sv[sv.len()-1] { sv.push(num); continue }
+        // search
+        let (mut lo, mut hi) = (0, sv.len()-1);
+        while lo < hi {
+            let mid = (lo+hi)/2;
+            let v = sv[mid];
+            if v == num { lo = mid;  break }
+            if v < num { lo = mid+1; }
+            else { hi = mid; }
+        }
+        sv[lo] = num;
+    }
+    sv.len() as i32
 }
 
 #[cfg(test)]
@@ -47,5 +70,13 @@ mod tests {
     #[test]
     fn it_works_06() {
         assert_eq!(length_of_lis(vec![1,3,6,7,9,4,10,5,6]), 6);
+    }
+    #[test]
+    fn it_works_07() {
+        assert_eq!(length_of_lis(vec![]), 0);
+    }
+    #[test]
+    fn it_works_08() {
+        assert_eq!(length_of_lis(vec![3,5,6,2,5,4,19,5,6,7,12]), 6);
     }
 }
