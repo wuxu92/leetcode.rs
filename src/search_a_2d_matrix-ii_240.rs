c7c6eb5a2f4abfed7a320a236ce70eefb4d85f98
wuxu92 <wuxu92@qq.com>
2019-02-26 12:13:56 +0800

    skip search_a_2d_matrix-ii_240 for no rust version now

---
+++ src/search_a_2d_matrix-ii_240.rs
@@ -0,0 +1,17 @@
+// @url https://leetcode.com/problems/search-a-2d-matrix-ii/
+// NOT RUST VERSION FOR NOW
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[test]
+    fn it_works_01() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
