48049f0ce138963a81a9318a5884bd789f5680d7
wuxu92 <wuxu92@qq.com>
2019-03-02 11:32:14 +0800

    finish find_all_numbers_disappeared_in_an_array_448 with two solutions

--- src/find_all_numbers_disappeared_in_an_array_448.rs
+++ src/find_all_numbers_disappeared_in_an_array_448.rs
@@ -1,7 +1,7 @@
 // @url https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
 #[allow(dead_code)]
-pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+pub fn find_disappeared_numbers_1(nums: Vec<i32>) -> Vec<i32> {
     let mut mark: Vec<bool> = vec![true; nums.len()+1];
     let mut res = vec![];
     for num in nums {
@@ -9,6 +9,20 @@ pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
     }
     for idx in 1..mark.len() {
         if mark[idx] { res.push(idx as i32);}
+    }
+    res
+}
+
+#[allow(dead_code)]
+pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+    let (mut nums, mut res) = (nums, vec![]);
+    for idx in 0..nums.len() {
+        let abs = if nums[idx] >= 0 { nums[idx] } else { -1 * nums[idx]};
+        let abs = abs as usize - 1;
+        nums[abs] = if nums[abs] < 0 { nums[abs] } else { -1 * nums[abs] }
+    }
+    for idx in 0..nums.len() {
+        if nums[idx] > 0 { res.push(idx as i32 + 1); }
     }
     res
 }
