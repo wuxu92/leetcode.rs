f9375a6c1871ef256d3377c4b5a8bca93eb39e3b
wuxu92 <wuxu92@qq.com>
2019-03-02 16:28:11 +0800

    add solution using dp for target_sum_494

--- src/target_sum_494.rs
+++ src/target_sum_494.rs
@@ -1,7 +1,7 @@
 // @url https://leetcode.com/problems/target-sum/
 
 #[allow(dead_code)]
-pub fn find_target_sum_ways(nums: Vec<i32>, s: i32) -> i32 {
+pub fn find_target_sum_ways_1(nums: Vec<i32>, s: i32) -> i32 {
     let (mut res, mut count) = (vec![], 0);
     for num in nums {
         if res.len() == 0 { res.push(num); res.push(-1 * num); continue; }
@@ -15,6 +15,30 @@ pub fn find_target_sum_ways(nums: Vec<i32>, s: i32) -> i32 {
     count
 }
 
+// subset P/N solution
+// sum(P) - sum(N) = s
+// sum(P) - sum(N) + sun(P) + sum(N) = s + sum(P) + sum(N) = s + sum(nums)
+// 2 * sum(p) = s + sum(nums)
+#[allow(dead_code)]
+pub fn find_target_sum_ways(nums: Vec<i32>, s: i32) -> i32 {
+    let sum : i32 = nums.iter().sum();
+    if sum < s || (sum + s) & 0b1 == 1 { return 0 }
+    let sum = (sum + s) as usize / 2;
+    // subset sum eq sum
+    let mut dp : Vec<i32> = vec![0; sum + 1];
+    dp[0] = 1;  // as give one more 0
+    for &num in nums.iter() {
+        let num = num as usize;
+        let mut idx = sum;
+        while idx >= num {
+            dp[idx] += dp[idx-num];
+            if idx == 0 { break }
+            idx -= 1;
+        }
+    }
+    dp[sum]
+}
+
 #[cfg(test)]
 mod tests {
     #[allow(unused_imports)]
@@ -22,6 +46,14 @@ mod tests {
     #[test]
     fn it_works_01() {
         assert_eq!(find_target_sum_ways(vec![1, 1, 1, 1, 1], 3), 5);
+    }
+    #[test]
+    fn it_works_02() {
+        assert_eq!(find_target_sum_ways(vec![1000], -1000), 1);
+    }
+    #[test]
+    fn it_works_03() {
+        assert_eq!(find_target_sum_ways(vec![0, 1], 1), 2);
     }
 }
 
