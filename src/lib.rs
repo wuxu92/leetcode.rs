e9033ce2e8c291b51cdfcc28245bee7b5ed697bd
wuxu92 <wuxu92@qq.com>
2019-03-12 20:27:14 +0800

    finish sliding_window_maximum_239 with a slow solution

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ mod utils;
 // mod sudoku_solver_37;
 // mod maximal_square_221;
 // mod product_of_array_except_self_238;
-// mod sliding_window_maximum_239;
+mod sliding_window_maximum_239;
 // mod perfect_squares_279;
 // mod move_zeroes_283;
 // mod find_the_duplicate_number_287;
@@ -74,7 +74,7 @@ mod utils;
 // mod search_a_2d_matrix_ii_240;
 // mod minimum_swaps_to_make_sequences_increasing_801;
 // mod maximum_length_of_repeated_subarray_718;
-mod minimum_size_subarray_sum_209;
+// mod minimum_size_subarray_sum_209;
 
 
 #[cfg(test)]
--- src/sliding_window_maximum_239.rs
+++ src/sliding_window_maximum_239.rs
@@ -3,7 +3,17 @@
 
 #[allow(dead_code)]
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    vec![]
+    if nums.len() == 0 || k == 0 { return vec![] }
+    let (mut res, len) = (nums.clone(), nums.len());
+    for i in 0..k as usize - 1 {
+        for j in 0..len-i-1 {
+            if res[j] < res[j+1] { res[j] = res[j+1]; }
+        }
+    }
+    for _i in 0..k-1 {
+        res.pop();
+    }
+    res
 }
 
 #[cfg(test)]
@@ -15,6 +25,27 @@ mod tests {
         assert_eq!(
             max_sliding_window(vec![1,3,-1,-3,5,3,6,7], 3),
             vec![3,3,5,5,6,7]
+        );
+    }
+    #[test]
+    fn it_works_02() {
+        assert_eq!(
+            max_sliding_window(vec![1,3,-1,-3,5,3,6,7], 1),
+            vec![1,3,-1,-3,5,3,6,7]
+        );
+    }
+    #[test]
+    fn it_works_03() {
+        assert_eq!(
+            max_sliding_window(vec![1,3,-1], 3),
+            vec![3]
+        );
+    }
+    #[test]
+    fn it_works_04() {
+        assert_eq!(
+            max_sliding_window(vec![], 0),
+            vec![]
         );
     }
 }
