e3a7f96650745c5199d78d2addccc1c31a6d55a1
wuxu92 <wuxu92@qq.com>
2019-03-13 16:06:44 +0800

    add slide window solution for permutation_in_string_567

--- src/permutation_in_string_567.rs
+++ src/permutation_in_string_567.rs
@@ -2,7 +2,7 @@
 
 //  if s2 contains the permutation of s1.
 #[allow(dead_code)]
-pub fn check_inclusion(s1: String, s2: String) -> bool {
+pub fn check_inclusion_1(s1: String, s2: String) -> bool {
     let len1 = s1.len();
     if s2.len() < s1.len() { return false }
     if len1 == 0 { return true }
@@ -29,6 +29,32 @@ pub fn check_inclusion(s1: String, s2: String) -> bool {
         }
         if map[idx] > 0 { map[idx] -= 1; count -= 1; }
         if count == 0 { return true }
+    }
+    false
+}
+
+// slide window
+#[allow(dead_code)]
+pub fn check_inclusion(s1: String, s2: String) -> bool {
+    let len1 = s1.len();
+    if len1 > s2.len() { return false }
+    let byte1 : Vec<_> = s1.bytes().collect();
+    let byte2 : Vec<_> = s2.bytes().collect();
+    let mut map : Vec<i32> = vec![0; 26];
+    let anum = 'a' as u8;
+    for i in 0..len1 {
+        map[(byte1[i] - anum) as usize] += 1;
+        map[(byte2[i] - anum) as usize] -= 1;
+    }
+    fn check_zero(m: &Vec<i32>) -> bool {
+        for n in m { if *n != 0 { return false } }
+        return true
+    }
+    if check_zero(&map) { return true }
+    for i in len1..byte2.len() {
+        map[(byte2[i-len1] - anum) as usize] += 1;
+        map[(byte2[i] - anum) as usize] -= 1;
+        if check_zero(&map) { return true }
     }
     false
 }
@@ -78,5 +104,17 @@ mod tests {
         let s1 = String::from("hello");
         let s2 = String::from("ooolleoooleh");
         assert_eq!(check_inclusion(s1, s2), false);
+    }
+    #[test]
+    fn it_works_08() {
+        let s1 = String::from("ab");
+        let s2 = String::from("a");
+        assert_eq!(check_inclusion(s1, s2), false);
+    }
+    #[test]
+    fn it_works_09() {
+        let s1 = String::from("ab");
+        let s2 = String::from("ab");
+        assert_eq!(check_inclusion(s1, s2), true);
     }
 }
